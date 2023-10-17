16b7197d75917a21a9d428a69fd20ea36bcb64b5
sheldor944 <mehrajul.abedin.944@gmail.com>
2023-10-17 19:20:21 +0600

    final

---
+++ app/src/main/java/com/example/dataencryptiontutorial/IntroductionActivity.java
@@ -0,0 +1,26 @@
+package com.example.dataencryptiontutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class IntroductionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction);
+        Button button = findViewById(R.id.btnStart);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext() , MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
